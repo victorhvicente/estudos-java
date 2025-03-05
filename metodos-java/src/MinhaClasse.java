7714de4d38906e9dd523e9978238f56ef0830238
vhvicente99@gmail.com <vhvicente99@gmail.com>
2025-03-05 14:49:41 -0300

    pasta metodos-java

---
+++ metodos-java/src/MinhaClasse.java
@@ -0,0 +1,30 @@
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+        System.out.println("Programa iniciado!");
+    }
+
+    public double somar(int n1, int n2) {
+        return n1 + n2; // Ou (double) (n1 + n2) se precisar ser double
+    }
+
+    public void imprimir(String texto) {
+        System.out.println(texto);
+    }
+
+    public double dividir(int dividendo, int divisor) throws Exception {
+        if (divisor == 0) {
+            throw new ArithmeticException("Divisão por zero não é permitida!");
+        }
+        return (double) dividendo / divisor;
+    }
+
+    private double metodoPrivado() {
+        return 0.0;
+    }
+
+    public void gravarCliente(Cliente cliente) {
+        System.out.println("Cliente " + cliente.getNome() + " gravado com sucesso!");
+    }
+}
+
